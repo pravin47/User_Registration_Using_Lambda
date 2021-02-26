2d4ac36023bacbd31f7c65d9fbe58df3789728fa
pravin bagul <bagul.pravin2@gmail.com>
2021-02-27 02:50:47 +0530

    Added:Welcome_Message

---
+++ User_Registration_Using_Lambda.java
@@ -0,0 +1,10 @@
+package com.user.registration.lambda;
+
+public class UserRegistrationUsingLambda {
+
+	public static void main(String args[]) {
+
+		System.out.println("Welcome To User Registration System");
+
+	}
+}
