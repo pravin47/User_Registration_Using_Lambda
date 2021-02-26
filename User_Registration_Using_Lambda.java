7529867a27b98b2a9e2ad1b7ef9154254ca40b4d
pravin bagul <bagul.pravin2@gmail.com>
2021-02-27 02:54:19 +0530

    Added:Validate_First_Name

--- User_Registration_Using_Lambda.java
+++ User_Registration_Using_Lambda.java
@@ -1,10 +1,32 @@
-package com.user.registration.lambda;
+package com.user.registration;
 
-public class UserRegistrationUsingLambda {
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistrationSystem {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void firstName() {
+
+		System.out.println("Enter The First Name");
+		String fName = sc.next();
+		String regex = "[A-Z]{1}[a-z]{2,}";
+		Pattern p1 = Pattern.compile(regex);
+		Matcher m1 = p1.matcher(fName);
+		System.out.println("String Result Is :" + m1.matches());
+		boolean check1 = (m1.matches());
+		if (check1 == true) {
+
+			System.out.println("First Name Is Valid");
+		} else {
+			System.out.println("First Name Is Invalid");
+		}
+	}
 
 	public static void main(String args[]) {
 
-		System.out.println("Welcome To User Registration System");
+		firstName();
 
 	}
 }
